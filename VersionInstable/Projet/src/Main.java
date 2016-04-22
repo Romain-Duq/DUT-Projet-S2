8f77d8e1222c99031c368ac77cd825588fbf6fc6
Romain Duquesne <duquesnr@ayou10.iut-infobio.priv.univ-lille1.fr>
2016-04-22 11:17:47 +0200

    versionI

--- VersionInstable/Projet/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-	public static void main(String[] args) {
-		DessinCarte.initImage();
-		new Partie();
-		//joueurBleu.avancerPersonne(Joueur.EXPLORATEUR);	
-		//fenetre.addPanel(dessinJoueur);
-	}
-}
